80c487a4838e83f6fde5b266808f71816d7128ce
Alex Kosm <adkosmachev@edu.hse.ru>
2018-02-17 15:12:14 +0300

    MJBC building simple prototype working with builed frontend

--- src/backend/application.py
+++ src/backend/application.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def root():
-    return render_template('index.html')
+def index_alias():
+    return send_from_directory('assets/web', 'index.html')
+
+
+@app.route('/<path:path>')
+def root_endpoint(path):
+    return send_from_directory('assets/web', path)
 
 app.run(host="0.0.0.0", port=8484)
